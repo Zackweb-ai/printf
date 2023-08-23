cfe13e76b293d423f052df30348347db7705fa84
warmflame <rashidasmoe@gmail.com>
2023-08-23 23:15:59 +0400

    main.h update

--- main.h
+++ main.h
@@ -5,25 +5,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-
-/**
- * struct format - Struct for format
- * @specifiers: Struct format
- * @f: The function associated
- */
-
-typedef struct specifiers
-{
+typedef struct specifiers{
 	char specifiers;
 	int (*f)(va_list);
 } specifiers_t;
 
-/*prototypes*/
 int _printf(const char *format, ...);
 int get_function(char s, va_list args);
 int _putchar(char c);
 
-/*Conversion specifiers*/
 int print_char(va_list args);
 int print_string(va_list args);
 int print_digit(va_list args);
