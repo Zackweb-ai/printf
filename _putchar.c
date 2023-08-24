99b822cc38827445cd86395ed7b21bb049fd6085
warmflame <rashidasmoe@gmail.com>
2023-08-24 18:42:01 +0100

    putchar and printf files updated

--- _putchar.c
+++ _putchar.c
@@ -1,5 +1,12 @@
-#include <unistd.h>
-int _putchar(char c) {
-	return (write(1, &c, 1));
-}
+#include "main.h"
 
+/**
+ * _putchar - prints/writes character
+ * @character: The character to print
+ * Return: 1, success, -1 error
+ */
+
+int _putchar(char character)
+{
+	return (write(1, &character, 1));
+}
